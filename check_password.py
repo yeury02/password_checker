3b0f94b0e0a60d2a5ab2d34f362d56f206fe3049
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-05-19 01:01:13 -0400

    finished project, program can tell you if your password has been used/hacked

--- check_password.py
+++ check_password.py
@@ -10,16 +10,28 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
     return res
 
-def read_res(response):
-    print(response.text)
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
-def pwned_api_heck(password):
-    sha1_password = (hashlib.sha1(password.encode('utf-8')).hexdigest().upper())
+def pwned_api_check(password):
+    sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first_5_chars, tail = sha1_password[:5], sha1_password[5:]
     response = request_api_data(first_5_chars)
-    print(first_5_chars, tail)
-    return read_res(response)
+    return get_password_leaks_count(response, tail)
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times... you should consider using a different password')
+        else:
+            print(f'{password} was not found. Carry on!')
+    return 'done!'
 
 if __name__ == '__main__':
-    pwned_api_heck('123')
+    main(sys.argv[1:])
     
